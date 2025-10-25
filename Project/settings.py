4c20c613d5a7520b508d25808184728b03a2d6a4
Nerbyshev Damir <cherebeevkarim2015@gmail.com>
2025-10-25 12:25:18 +0300

    before DRF
    + added deps

--- Project/settings.py
+++ Project/settings.py
@@ -8,15 +8,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+THIRD_PARTY_APPS = ["rest_framework", "django_filters"]
+LOCAL_APPS = [
     "api",
 ]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
